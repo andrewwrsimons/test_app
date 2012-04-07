d089bea5d0632c78442bf907ec81c56c388899c3
Andrew Simons <andrewwrsimons@gmail.com>
2012-04-07 18:06:26 -0300

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcglApp::Application.config.secret_token = '3be6d3f0054001d5899b54f31256f895546e6df8557268267f07b1159db07a2f8c0c46c0dff01c4402a018e9dc9fbc409461666ed9d96e7438a4eb0a945f5113'
